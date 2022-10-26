5cadb03be1408595ea9e14d081f17d95be97ddc4
Peter Murray <681306+peter-murray@users.noreply.github.com>
2022-10-26 18:04:32 +0000

    Updating building of executables to use pkg

--- src/executable/cli.ts
+++ src/executable/cli.ts
@@ -1,31 +1,45 @@
-import { Snapshot, submitSnapshot } from '@github/dependency-submission-toolkit';
-import { generateSnapshot } from '../snapshot-generator';
 import pkg from '../../package.json';
 
 const { program } = require('commander');
-program.name('maven-dependency-submission');
+program.name(pkg.name);
 program.version(pkg.version);
 
 program.requiredOption('-t, --token <token>', 'GitHub access token');
 program.requiredOption('-r --repository <repository>', 'GitHub repository, owner/repo_name format');
-program.requiredOption('-b --branch-ref <ref>', 'GitHub repository branch reference, e.g. /refs/heads/main');
+program.requiredOption('-b --branch-ref <ref>', 'GitHub repository branch reference, e.g. refs/heads/main');
 program.requiredOption('-s --sha <commitSha>', 'GitHub repository commit SHA');
 
 program.option('-d --directory <maven-project-directory>', 'the directory containing the Maven POM file', '.');
+program.option('--settings-file <settings-file>', 'path to the Maven settings file');
+program.option('--ignore-maven-wrapper', 'ingore Maven wrappers, if present, and use Maven from the PATH');
+program.option('--maven-args <maven-args>', 'additional arguments to pass to Maven');
 program.option('--github-api-url <url>', 'GitHub API URL', 'https://api.github.com');
 program.option('-j --job-name <jobName>', 'Optional name for the activity creating and submitting the graph', 'maven-dependency-submission-cli');
 program.option('-i --run-id <jobName>', 'Optional Run ID number for the activity that is providing the graph');
 
 program.parse(process.argv);
 
+const opts = program.opts();
+
+// Inject some required environment variables like the Actions INPUTs and special environment variables
+process.env['INPUT_TOKEN'] = opts.token;
+process.env['GITHUB_REPOSITORY'] = opts.repository;
+process.env['GITHUB_API_URL'] = opts.githubApiUrl;
+
+// The above injection of environment variables is required before the submission APIs are imported
+import { Snapshot, submitSnapshot } from '@github/dependency-submission-toolkit';
+import { generateSnapshot } from '../snapshot-generator';
+
 async function execute() {
-  const opts = program.opts();
-
-  // Inject some required environment variables like the Actions INPUTs and special environment variables
-  process.env['INPUT_TOKEN'] = opts.token;
-  process.env['GITHUB_REPOSITORY'] = opts.repository;
-
   let snapshot: Snapshot | undefined;
+
+  // The dependency submission API requires a formatted ref reference so check early fo now
+  if (`/^refs\//`.match(opts.branchRef) === null) {
+    console.error(`Branch reference must be in path form, e.g. 'refs/heads/main' for the 'main' branch.`);
+    program.help({ error: true });
+    process.exit(1);
+  }
+
   try {
     // Build a fake GitHub Actions context so that values for the submission APIs can be retrieved
     const context = {
@@ -38,7 +52,12 @@ async function execute() {
       id: `${opts.runId || Date.now()}`
     };
 
-    const mvnConfig = {};
+    const mvnConfig = {
+      directory: opts.directory,
+      settingsFile: opts.settingsFile,
+      ignoreMavenWrapper: opts.ignoreMavenWrapper,
+      mavenArgs: opts.mavenArgs
+    };
 
     snapshot = await generateSnapshot(opts.directory, mvnConfig, context, job);
 
