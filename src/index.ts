58c3f7c8c55ff6b76ad2c61b1e7dcd7344ed4672
jmservera <8036360+jmservera@users.noreply.github.com>
2023-10-31 09:09:52 +0000

    use sha and ref if present

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,11 @@
 import * as core from '@actions/core';
-import {Snapshot, submitSnapshot} from '@github/dependency-submission-toolkit';
+import * as github from '@actions/github';
+import { Snapshot, submitSnapshot } from '@github/dependency-submission-toolkit';
 import { generateSnapshot } from './snapshot-generator';
 
 async function run() {
   let snapshot: Snapshot | undefined;
+  let context: any | undefined;
 
   try {
     const directory = core.getInput('directory', { required: true });
@@ -15,7 +17,24 @@ async function run() {
     const includeFilename = core.getBooleanInput('snapshot-include-file-name');
     const manifestFilename = core.getInput('snapshot-dependency-file-name');
 
-    snapshot = await generateSnapshot(directory, mavenConfig, {includeManifestFile: includeFilename, manifestFile: manifestFilename});
+    if (core.getInput('sha') || core.getInput('ref')) {
+      // build synthetic context when sha and ref are provided
+      if (!core.getInput('sha')) {
+        throw ('sha is required when providing ref');
+      }
+      if (!core.getInput('ref')) {
+        throw ('ref is required when providing sha');
+      }
+
+      context = {
+        repo: github.context.repo,
+        sha: core.getInput('sha'),
+        ref: core.getInput('ref'),
+        eventName: '' // usually github.context.eventName, let it empty so the sdk uses the provided sha and ref
+      }
+    }
+
+    snapshot = await generateSnapshot(directory, mavenConfig, { includeManifestFile: includeFilename, manifestFile: manifestFilename, context: context });
   } catch (err: any) {
     core.error(err);
     core.setFailed(`Failed to generate a dependency snapshot, check logs for more details, ${err}`);
@@ -27,7 +46,7 @@ async function run() {
     core.endGroup();
 
     core.info(`Submitting Snapshot...`)
-    await submitSnapshot(snapshot);
+    await submitSnapshot(snapshot, context);
     core.info(`completed.`)
   }
 }
