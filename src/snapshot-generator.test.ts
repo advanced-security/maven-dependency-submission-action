2ba839e04be286a1fc7f93ae4a9683f0e1ec335f
David Wiggs <107562400+david-wiggs@users.noreply.github.com>
2024-07-22 23:27:03 +0000

    Add some tests

--- src/snapshot-generator.test.ts
+++ src/snapshot-generator.test.ts
@@ -1,4 +1,4 @@
-import { getMavenProjectDirectory } from './utils/test-util';
+import { getMavenProjectDirectory, getMavenSettingsFile } from './utils/test-util';
 import { generateDependencyGraph, generateSnapshot } from './snapshot-generator';
 import {describe, it, expect} from 'vitest';
 
@@ -68,5 +68,42 @@ describe('snapshot-generator', () => {
       expect(snapshot.detector.version).toBe(version);
       expect(snapshot.manifests['problem-dependency-graph-2602'].countDependencies()).toBe(230);
     }, 40000);
+
+    it('should append correlator from snapshotConfig if it exists', async() => {
+      const projectDir = getMavenProjectDirectory('simple');
+      const mavenSettingsFile = getMavenSettingsFile();
+      const mavenConfig = {
+        ignoreMavenWrapper: true,
+        settingsFile: mavenSettingsFile,
+        mavenArgs: '-DskipTests'
+      };
+      const snapshotConfig = {
+        correlator: 'configCorrelator',
+        job: {
+          correlator: 'jobCorrelator'
+        }
+      };
+      const snapshot = await generateSnapshot(projectDir, mavenConfig, snapshotConfig);
+
+      expect(snapshot.job.correlator).toBe('jobCorrelator-configCorrelator');
+    }, 20000);
+
+    it('should use existing job correlator if snapshotConfig correlator does not exist', async() => {
+      const projectDir = getMavenProjectDirectory('simple');
+      const mavenSettingsFile = getMavenSettingsFile();
+      const mavenConfig = {
+        ignoreMavenWrapper: true,
+        settingsFile: mavenSettingsFile,
+        mavenArgs: '-DskipTests'
+      };
+      const snapshotConfig = {
+        job: {
+          correlator: 'jobCorrelator'
+        }
+      };
+      const snapshot = await generateSnapshot(projectDir, mavenConfig, snapshotConfig);
+
+      expect(snapshot.job.correlator).toBe('jobCorrelator');
+    }, 20000);
   });
 });
